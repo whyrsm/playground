a7bb37da1efd9d9d6c8e2e45ac379870151f5a66
Wahyu Rismawan <wahyurismawan@gmail.com>
2024-12-14 06:10:02 +0700

    binance integration: get user balance and price in usd

--- src/exchanges/exchanges-binance.service.ts
+++ src/exchanges/exchanges-binance.service.ts
@@ -4,6 +4,7 @@ import * as ccxt from 'ccxt';
 @Injectable()
 export class ExchangesBinanceService {
   private binance;
+  private BINANCE: string = 'BINANCE';
 
   constructor() {
     this.binance = new ccxt.binance({
@@ -12,22 +13,43 @@ export class ExchangesBinanceService {
     });
   }
 
-  async fetchBalance(coins: string[]): Promise<any> {
-    try {
-      const balance = await this.binance.fetchBalance();
-      return coins.map((coin) => ({
-        asset: coin.toUpperCase(),
-        balance: balance.total[coin.toUpperCase()] || 0,
-      }));
-    } catch (error) {
-      throw new Error(`Failed to fetch balance: ${error.message}`);
-    }
+  private formatNumber(value: number): string {
+    return value.toFixed(9);
   }
 
-  async fetchTransactions() {
+  async fetchBalance(coins: string[]): Promise<any> {
     try {
-      const trades = await this.binance.fetchMyTrades();
-      return trades;
+      // Get user balances
+      const balance = await this.binance.fetchBalance();
+
+      // create trading pairs for each coin
+      const tradingPairs = coins
+        .filter((coin) => coin.toUpperCase() !== 'USDT')
+        .map((coin) => `${coin}\USDT`);
+
+      // fetch tickers for the traiding pairs
+      const tickers = await this.binance.fetchTickers(tradingPairs);
+
+      return coins.map((coin) => {
+        const normalizedCoin: string = coin.toUpperCase();
+        const balanceAmount = balance.total[normalizedCoin] || 0;
+        // handle USD price
+        const usdPrice =
+          normalizedCoin === 'USDT'
+            ? 1
+            : tickers[`${normalizedCoin}/USDT`]?.last || 0;
+
+        const usdValue = balanceAmount * usdPrice;
+
+        return {
+          asset: normalizedCoin,
+          balance: this.formatNumber(balanceAmount),
+          value: {
+            usd: this.formatNumber(usdValue),
+          },
+          source: this.BINANCE,
+        };
+      });
     } catch (error) {
       throw new Error(`Failed to fetch balance: ${error.message}`);
     }
