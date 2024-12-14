bcc76cce8bad4282f2e76ef17e5b657f4db9e4b9
Wahyu Rismawan <wahyurismawan@gmail.com>
2024-12-14 07:24:07 +0700

    API /all/balance: make a summary of total asset

--- src/exchanges/exchanges.controller.ts
+++ src/exchanges/exchanges.controller.ts
@@ -1,14 +1,40 @@
 import { Controller, Get } from '@nestjs/common';
-// import { ExchangesBinanceService } from './exchanges-binance.service';
+import { ExchangesBinanceService } from './exchanges-binance.service';
 import { ExchangesHyperliquidService } from './exchanges-hyperliquid.service';
 
 @Controller('exchanges')
 export class ExchangesController {
-  constructor(private readonly hyperliquid: ExchangesHyperliquidService) {}
+  constructor(
+    private readonly hyperliquid: ExchangesHyperliquidService,
+    private readonly binance: ExchangesBinanceService,
+  ) {}
 
-  @Get('balance')
+  private BINANCE_ASSET: string[] = ['BTC', 'ETH', 'SOL', 'BNB', 'USDT', 'USDC', 'NOT']
+  private HYPERLIQUID_ASSET: string[] = ['HYPE', 'USDC']
+
+  @Get('hyperliquid/balance')
+  async getHyperliquidBalance() {
+    return this.hyperliquid.fetchBalance(this.HYPERLIQUID_ASSET);
+  }
+
+  @Get('binance/balance')
+  async getBinanceBalance() {
+    return this.binance.fetchBalance(this.BINANCE_ASSET);
+  }
+
+  @Get('all/balance')
   async getBalance() {
-    return this.hyperliquid.fetchBalance(['HYPE', 'USDC']);
-    // return this.binance.fetchBalance(['BTC', 'ETH', 'USDT', 'USDC', 'SOL']);
+    const assetHyperliquid = await this.hyperliquid.fetchBalance(this.HYPERLIQUID_ASSET);
+    const assetBinance = await this.binance.fetchBalance(this.BINANCE_ASSET);
+    const assetAll = [...assetHyperliquid, ...assetBinance];
+    const totalUSDValue = assetAll.reduce(
+      (acc, balance) => acc + parseFloat(balance.value.usd),
+      0,
+    );
+
+    return {
+      total_usd_value: totalUSDValue,
+      assets: assetAll,
+    };
   }
 }
